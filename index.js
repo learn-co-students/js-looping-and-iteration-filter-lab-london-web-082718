a523e1e71a172e6e358a82a8622687e922310ac4
github username <github email address>
2018-10-08 15:09:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(driverArray, nameString){
+    drivers = driverArray.filter(n => n.toLowerCase() === nameString.toLowerCase())
+    return drivers
+}
+    
+function fuzzyMatch(driverArray, nameString){
+
+    drivers = driverArray.filter(n => n.slice(0, nameString.length) === nameString )
+    return drivers
+}
+
+
+function matchName(driverArray, nameString){
+    drivers = driverArray.filter(n => n.name === nameString )
+    return drivers
+}
