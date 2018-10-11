d58c8d40c941ef97c3ed8bb144d37794846e4c3c
braj011 <bhagyaraj01@gmail.com>
2018-10-11 15:57:49 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file
+
+// array of drivers 
+
+function findMatching(array, name) {
+    return array.filter( function(driverName) {      
+        // driverName is just the 'placeholder' name to see if the elements in array ...
+        // matches the name / string parameter passed in 
+        return driverName.toLowerCase() === name.toLowerCase();
+
+    });
+}
+
+function fuzzyMatch(array, partialName) {
+
+    return array.filter( function(name) {      
+        return name.startsWith(partialName);
+    });
+}
+function matchName(array, string) {
+    // each element of array is an object, these objects have a property of name 
+    // function needs to return each element whose name property matches provided string
+
+    // we know that each object has a property name - so can use dot notation to access
+    return array.filter( function(driverName) { 
+        return driverName.name === string;
+    });
+}
