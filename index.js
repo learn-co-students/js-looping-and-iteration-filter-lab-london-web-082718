aa84ad9ca9e3feb34ffb2d3ca29bbab6c1cd2e6d
Billiesnyc <billiesnyc@gmail.com>
2018-10-11 11:08:53 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers, name){
+    return drivers.filter(function (driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, letter){
+    letterLength = letter.length
+    return drivers.filter(function (beginsWith){
+        return beginsWith.toLowerCase().slice(0, letterLength) === letter.toLowerCase();
+    });
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(nameMatch){
+        return nameMatch.name.toLowerCase() === name.toLowerCase();
+    });
+}
